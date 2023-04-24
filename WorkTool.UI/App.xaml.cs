4537cd1f086b4932f5f4fa6010bc18dee17aad71
Bogomaz Dmitry <d.bogomaz@itransition.com>
2023-04-24 12:49:52 +0300

    init commit

---
+++ WorkTool.UI/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace WorkTool.UI;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ WorkTool.UI/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace WorkTool.UI;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
