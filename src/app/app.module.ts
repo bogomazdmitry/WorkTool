c658c43f127941021b36df308f34da4fd055fab7
Bogomaz Dmitry <bogomaz.dima2013@gmail.com>
2023-07-09 17:45:42 +0300

    added pwa support

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
@@ -9,6 +9,7 @@ import { ChatGptModule } from './chat-gpt/chat-gpt.module';
 import { TextModule } from './text/text.module';
 import { JsonPipe } from '@angular/common';
 import { RedirectComponent } from './redirect.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [AppComponent, RedirectComponent],
@@ -19,6 +20,12 @@ import { RedirectComponent } from './redirect.component';
     SharedModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideFirestore(() => getFirestore()),
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [JsonPipe],
   bootstrap: [AppComponent],
