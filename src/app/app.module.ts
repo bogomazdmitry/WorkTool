69a3c6cd58316ef62f3276efce1400a32b16401e
Bogomaz Dmitry <bogomaz.dima2013@gmail.com>
2023-07-09 17:51:51 +0300

    linter apply

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,7 +24,7 @@ import { ServiceWorkerModule } from '@angular/service-worker';
       enabled: !isDevMode(),
       // Register the ServiceWorker as soon as the application is stable
       // or after 30 seconds (whichever comes first).
-      registrationStrategy: 'registerWhenStable:30000'
+      registrationStrategy: 'registerWhenStable:30000',
     }),
   ],
   providers: [JsonPipe],
